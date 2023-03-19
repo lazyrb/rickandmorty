1a80664382c3f36049915716bfe9339b895e826c
Anton Zhuravlov <zhuravlov1999@gmail.com>
2023-03-19 14:45:55 +0200

    firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterComponent } from './components/characterDetails/character.details.component';
 import { SearchComponent } from './components/search/search.component';
+import { SignInComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [		
     AppComponent,
     SearchComponent,
     CharacterComponent,
+    SignInComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     HttpClientModule,
     FormsModule
   ],
